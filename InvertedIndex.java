3c7ff8b766ce9d40d43ecb2938d1164b63a46262
Chris <cxyznj@163.com>
2019-04-25 15:50:08 +0800

    Update InvertedIndex.java

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -1,3 +1,5 @@
+package com.myschool.hadoop;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -13,114 +15,144 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.StringTokenizer;
+import java.util.Vector;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Enumeration;
+import java.util.Map;
 
-public class InvertedIndex {
-    public static class InvertedIndexMapper
+public class InvertedIndexer {
+
+    // map class extend Mapper
+    public static class Map
         extends Mapper<Object, Text, Text, IntWritable> {
 
-        private final static IntWritable intValue = new IntWritable();
-        private final static Text textKey = new Text();
+        private static Text Key_word = new Text();
+        private static IntWritable Value_int = new IntWritable();
 
+        // override map function
         @Override
-        protected void map(Object key, Text value, Context content)
-                throws IOException, InterruptedException {
+        protected void map(Object key, Text value, Context content) throws IOException, InterruptedException {
 
             FileSplit fileSplit = (FileSplit)content.getInputSplit();
-            String fileName = fileSplit.getPath().getName().split("(.txt)|(.TXT)")[0];
+            // Get the filename of files. According to requirements, we need remove the suffix from the filename
+            String fileName = fileSplit.getPath().getName();
+            if(fileName.length() > 14) {
+                fileName = fileName.substring(0, fileName.length()-14);
+            }
 
-            /* Count the frequency of words that appear in a line. */
-            Hashtable ht = new Hashtable<String, Integer>();
+            //Count the frequency of words that appear in a line.
             StringTokenizer itr = new StringTokenizer(value.toString());
             while(itr.hasMoreTokens()){
                 String word = itr.nextToken();
-                if(ht.containsKey(word)){
-                    ht.put(word, new Integer(((Integer)ht.get(word)).intValue()+1));
-                }
-                else{
-                    ht.put(word, new Integer(1));
-                }
+                Key_word.set(word+","+fileName);
+                Value_int.set(1);
+                content.write(Key_word, Value_int);
+            }
+        }
+
+        static class WordCount {
+            private String word;
+            private int count;
+
+            public WordCount() {
+                super();
             }
 
-            Enumeration<String> words = ht.keys();
-            while(words.hasMoreElements()){
-                String word = words.nextElement();
-                textKey.set(word+","+fileName);
-                intValue.set(((Integer)ht.get(word)).intValue());
-                content.write(textKey, intValue);
+            public String getWord() {
+                return word;
+            }
+            public void setWord(String Word) {
+                this.word = Word;
+            }
+
+            public int getCount() {
+                return count;
+            }
+            public void setCount(int Count) {
+                this.count = Count;
             }
         }
     }
 
-    public static class LineCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+    // combine class extend Reducer, cause we need to combine same word in same file, and let them as one key
+    public static class Combiner extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-        private IntWritable intValue = new IntWritable();
+        private IntWritable Value_int = new IntWritable();
 
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException {
 
-            /* Count the frequency of words that appear in a file. */
+            // Totally number of the word appear in a specific file
+            //Value_int.set(0);
             int sum = 0;
+
             for(IntWritable value : values)
                 sum += value.get();
+                //Value_int.set(Value_int.get() + value.get());
 
-            intValue.set(sum);
-            context.write(key, intValue);
+            Value_int.set(sum);
+            context.write(key, Value_int);
         }
     }
 
+    // Change the key of patition, use default patition function
     public static class WordPartition extends HashPartitioner<Text, Object> {
 
-        private final static Text word = new Text();
+        private static Text keyword = new Text();
         @Override
         public int getPartition(Text key, Object value, int numReduceTasks) {
-            word.set(key.toString().split(",")[0]);
-            return super.getPartition(word, value, numReduceTasks);
+
+            keyword.set(key.toString().split(",")[0]);
+
+            return super.getPartition(keyword, value, numReduceTasks);
         }
     }
 
-    public static class InvertedIndexReducer extends Reducer<Text, IntWritable, Text, Text> {
+    public static class Reduce extends Reducer<Text, IntWritable, Text, Text> {
 
-        private static String prevWord = null;
-        private static int sum_of_frequency = 0;     // sum of frequency of words in all files
-        private static int num_of_file = 0;         //  num of files containing the word
-        StringBuilder postings = new StringBuilder(); //format : filename1:frequency1;...
+        private static Text Key_word = new Text();
+        private static Text textValue = new Text();
 
-        private final static Text textKey = new Text();
-        private final static Text textValue = new Text();
+        private static String curWord = null;
+        private static int appear_num = 0;
+        private static int file_num = 0;
+        StringBuffer ValueText = new StringBuffer();
+
         @Override
-        protected void reduce(Text key, Iterable<IntWritable> values, Context context)
-                throws IOException, InterruptedException {
-            String[] word_filename = key.toString().split(",");
-            if(prevWord!=null && !word_filename[0].equals(prevWord)){       // New word
-                textKey.set(prevWord);
-                float average = (float) sum_of_frequency / num_of_file ;
-                textValue.set(average+","+postings.toString());
-                context.write(textKey, textValue);
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // split word and filename
+            String[] source = key.toString().split(",");
+            String word = source[0];
+            String filename = source[1];
 
-                sum_of_frequency = num_of_file = 0;
-                postings.delete(0, postings.length());
+            // when one word's calculate is end, calculate the average appear number.
+            if(curWord != null && (word.equals(curWord) == false)){
+                Key_word.set(curWord);
+                float average = (float) appear_num / file_num ;
+                textValue.set(average+","+ValueText.toString());
+                context.write(Key_word, textValue);
+
+                file_num = 0;
+                appear_num = 0;
+                ValueText.delete(0, ValueText.length());
             }
-            int num_of_frequency = 0;   // num of frequency of word in this file
-            for(IntWritable value : values)     // values.length() == 1 ?
-                num_of_frequency += value.get();
-            num_of_file ++ ;
-            sum_of_frequency += num_of_frequency;
-            if(num_of_file != 1)
-                postings.append(";");
-            postings.append(word_filename[1]+":"+num_of_frequency);
 
-            prevWord = word_filename[0];
-        }
+            curWord = word;
 
-        @Override
-        protected void cleanup(Context context) throws IOException, InterruptedException {
-            textKey.set(prevWord);
-            float average = (float) sum_of_frequency / num_of_file ;
-            textValue.set(average+","+postings.toString());
-            context.write(textKey, textValue);
+            int frequency = 0;
+            for(IntWritable value : values)
+                frequency += value.get();
+                file_num += 1;
+            appear_num += frequency;
+
+            if(file_num > 1) ValueText.append(";");
+
+            ValueText.append(filename+":"+frequency);
         }
     }
 
@@ -128,16 +160,16 @@ public class InvertedIndex {
 
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "inverted index");
+        job.setJarByClass(InvertedIndexer.class);
 
-        job.setJarByClass(InvertedIndex.class);
-
-        job.setMapperClass(InvertedIndexMapper.class);
-        job.setCombinerClass(LineCombiner.class);
-        job.setPartitionerClass(WordPartition.class);
-        job.setReducerClass(InvertedIndexReducer.class);
-
+        job.setMapperClass(Map.class);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
+
+        job.setCombinerClass(Combiner.class);
+        job.setPartitionerClass(WordPartition.class);
+
+        job.setReducerClass(Reduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
